211b908c1ebf52e5eb65e46bed89fade68b1f2a2
zhangyongping <zhangyongping@tezign.com>
2020-04-27 11:21:51 +0800

    init dataway

---
+++ dataway/src/main/java/com/trickly/dataway/DatawayApplication.java
@@ -0,0 +1,13 @@
+package com.trickly.dataway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatawayApplication.class, args);
+    }
+
+}
---
+++ dataway/src/test/java/com/trickly/dataway/DatawayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trickly.dataway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
